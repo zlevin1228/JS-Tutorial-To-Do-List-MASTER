6a42bfdfff2133354111d402b40a575696c6d202
Zachary Levin <47122362+zlevin1228@users.noreply.github.com>
2024-05-13 18:24:31 +0000

    store to do section complete

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const clear = document.querySelector(".clear")
 const dateElement = document.getElementById("date")
 const list = document.getElementById("list")
 const input = document.getElementById("input")
+let LIST = [];
+let id = 0;
 
 function addToDo (toDo) {
     const list = document.getElementById("list")
@@ -21,8 +23,15 @@ document.addEventListener("keyup",function(event){
     if(event.keyCode == 13){
         const toDo = input.value
         if(toDo){
-            addToDo(toDo)
+            addToDo(toDo, id, false, false)
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
+            })
         }
         input.value = ""
+        id++
     }
 })
