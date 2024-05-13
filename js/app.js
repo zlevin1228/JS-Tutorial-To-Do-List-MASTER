8736983f63a3f21dbc12394f746a0a1a59942b17
Zachary Levin <47122362+zlevin1228@users.noreply.github.com>
2024-05-13 18:20:16 +0000

    to do section complete

--- js/app.js
+++ js/app.js
@@ -4,6 +4,25 @@ const dateElement = document.getElementById("date")
 const list = document.getElementById("list")
 const input = document.getElementById("input")
 
-const element = document.getElementById("element")
-element.insertAdjacentHTML(position, text)
-element.innerHTML(text)
+function addToDo (toDo) {
+    const list = document.getElementById("list")
+    const text = `
+<li class="item">
+    <i class="co fa fa-circle-thin" job="complete"></i>
+    <p class="text">${toDo}</p>
+    <i class="de fa fa-trash-o" job="delete"></i>
+</li>
+`
+    const position = "beforeend"
+    list.insertAdjacentHTML(position, text)
+}
+
+document.addEventListener("keyup",function(event){
+    if(event.keyCode == 13){
+        const toDo = input.value
+        if(toDo){
+            addToDo(toDo)
+        }
+        input.value = ""
+    }
+})
