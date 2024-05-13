692380e70dc4131c6b5f8fa0265b85e4e9a8e20e
Zachary Levin <47122362+zlevin1228@users.noreply.github.com>
2024-05-13 18:13:51 +0000

    insertAdjacentHTML() section done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 // CODE EXPLAINED channel
+const clear = document.querySelector(".clear")
+const dateElement = document.getElementById("date")
+const list = document.getElementById("list")
+const input = document.getElementById("input")
+
+const element = document.getElementById("element")
+element.insertAdjacentHTML(position, text)
+element.innerHTML(text)
