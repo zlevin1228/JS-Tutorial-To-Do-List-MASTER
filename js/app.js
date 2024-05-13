8145cdb5e7e2c19b5cc28a8cf96aaac94a83ffbd
Zachary Levin <47122362+zlevin1228@users.noreply.github.com>
2024-05-13 19:00:40 +0000

    stopped at 44:03

--- js/app.js
+++ js/app.js
@@ -1,43 +1,43 @@
 // CODE EXPLAINED channel
+
+//Select the elements
 const clear = document.querySelector(".clear")
 const dateElement = document.getElementById("date")
 const list = document.getElementById("list")
 const input = document.getElementById("input")
-let LIST = [];
-let id = 0;
+
+// Classes names
 const CHECK = "fa-check-circle"
 const UNCHECK = "fa-circle-thin"
 const LINE_THROUGH = 'lineThrough'
 
-function addToDo (toDo, id, done,) {
+// variables
+let LIST = [], id = 0
+
+// Show todays date
+const options = { weekday:'long', month:'short', day:'numeric'}
+const today = new Date()
+dateElement.innerHTML = today.toLocaleDateString("en-US", options)
+
+// add to do function
+function addToDo (toDo, id, done, trash) {
     if(trash){
         return
     }
-    const DONE = done ? CHECK : UNCHECK;
+    const DONE = done ? CHECK : UNCHECK
     const LINE = done ? LINE_THROUGH : ""
-    const text = `
+    const item = `
 <li class="item">
-    <i class="fa ${DONE} complete" job="complete" id="${id}"></i>
+    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
     <p class="text ${LINE}">${toDo}</p>
-    <i class="fa fa-trash-o delete" job="delete" id="${id}"></i>
+    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
 </li>
 `
     const position = "beforeend"
     list.insertAdjacentHTML(position, text)
 }
 
-function completeToDo(element){
-    element.classList.toggle(CHECK)
-    element.classList.toggle(UNCHECK)
-    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH)
-    LIST[element.id].done = LIST[element.id].done ? false : true
-}
-
-function removeToDo(element){
-    element.parentNode.parentNode.removeChild(element.parentNode)
-    LIST[element.id].trash = true
-}
-
+// add an item to the list user the enter key
 document.addEventListener("keyup",function(event){
     if(event.keyCode == 13){
         const toDo = input.value
@@ -55,13 +55,28 @@ document.addEventListener("keyup",function(event){
     }
 })
 
+// complete to do
+function completeToDo(element){
+    element.classList.toggle(CHECK)
+    element.classList.toggle(UNCHECK)
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH)
+    LIST[element.id].done = LIST[element.id].done ? false : true
+}
+
+// remove to do
+function removeToDo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode)
+    LIST[element.id].trash = true
+}
+
+// target the items created dynamically
 list.addEventListener("click", function(event){
-    let element = event.target
+    const element = event.target
     const elementJOB = event.target.attributes.job.value
     if(elementJOB == "complete"){
         completeToDo(element)
     }
-    if(elementJOB == "delete"){
+    if(elementJOB == "remove"){
         removeToDo(element)
     }
 })
@@ -91,9 +106,3 @@ clear.addEventListener('click', function(){
     localStorage.clear()
     location.reload()
 })
-
-let options = { weekday:'long', month:'short', day:'numeric'}
-
-let today = new Date()
-
-dateElement.innerHTML = today.toLocaleDateString("en-US", options)
